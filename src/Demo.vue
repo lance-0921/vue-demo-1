47a66153380909c78fe1685de21756534711f00c
lance <861936154@qq.com>
2021-10-04 15:08:02 +0800

    vue-demo-1

---
+++ src/Demo.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="red">
+    {{ n }}
+    <button @click="add">+1</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    //vue.loader的data只支持函數
+    return {
+      n: 0,
+    };
+  },
+  methods: {
+    add() {
+      return (this.n += 1);
+    },
+  },
+};
+</script>
+
+<style>
+.red {
+  color: red;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
-import App from './App.vue'
+
 
 Vue.config.productionTip = false
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+import Demo from './Demo.vue'
+
+new Vue (
+  {
+    el:"#app",
+   
+    render(h){
+      return h(Demo)
+    },
+  })
